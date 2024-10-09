61e6568e09e756503169e16b83eea4d130c0246c
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2024-10-09 17:01:39 +0200

    clock

--- 04_JavaScript_Canvas_Clock/main_js.mjs
+++ 04_JavaScript_Canvas_Clock/main_js.mjs
@@ -15,28 +15,53 @@ window.onload = () => {
 
     // Haupt-Zeichnen-Funktion
     function draw() {
+        const now = new Date();
+        let hours = now.getHours();
+        if (hours > 12) hours -= 12;
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+
+
         ctx.resetTransform();   // Zurücksetzen der Transformation
         ctx.clearRect(0, 0, canvas.width, canvas.height); // Löschen der Zeichnung wg. Animation
 
         // 1. Verschiebung in die Mitte des Bildschirms
         ctx.translate(canvas.width / 2, canvas.height / 2);
-
         ctx.rotate(-2 * hourAngle);
 
-        // 2. Schleife über 12 Einheiten / Stunden
-        for (let i = 0; i < 12; ++i) {
-            // 4. Zeichne schmales Rechteck
-            ctx.fillRect(radius, 0, size, 2);
+        ctx.save();
+        ctx.fillStyle = "#f00";
+        ctx.rotate(-hourAngle + seconds * Math.PI / 30);
+        ctx.fillRect(0, 0, radius + size, 1);
+        ctx.restore();
 
+        ctx.save();
+        ctx.fillStyle = "#000";
+        ctx.rotate(-hourAngle + minutes * Math.PI / 30);
+        ctx.fillRect(0, 0, radius + size, 3);
+        ctx.restore();
+
+        ctx.save();
+        ctx.fillStyle = "#000";
+        ctx.rotate(-hourAngle + hours * Math.PI / 6);
+        ctx.fillRect(0, 0, radius, 6);
+        ctx.restore();
+
+        ctx.save();
+        ctx.fillStyle = "#000";
+        for (let i = 0; i < 12; ++i) {
+            ctx.fillRect(radius, 0, size, 1);
             ctx.save();
-            ctx.translate(radius + 2 * size, 0);
+            ctx.translate(radius + size + 5, 0);
             ctx.rotate(Math.PI / 2 - (i + 1) * hourAngle);
             ctx.fillText(`${i + 1}`, 0, 0);
             ctx.restore();
-
-            // 3. Drehe um 30 Grad = 360 / 12; 2PI / 12 = Math.PI / 12;
             ctx.rotate(hourAngle);
         }
+        ctx.restore();
+
+
+
 
         window.requestAnimationFrame(draw);
     }
