519471e77bfa47176fc6df28a277dfd92de4345e
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2024-10-30 10:40:48 +0100

    pre 30.10.2024

---
+++ 06_Grab_two_Fingers/js/canvas_funcs.mjs
@@ -0,0 +1,326 @@
+
+
+// Parameter: id:string des HTML-Canvas
+export function init(id) {
+    const canvas = document.getElementById(id);
+    // console.log(canvas);
+    const ctx = canvas.getContext("2d");  // 
+
+    function resize() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        // console.log(`${canvas.width}x${canvas.height}`)
+    }
+    resize();
+    window.addEventListener("resize", resize);
+
+    return { canvas, ctx };
+}
+
+function distance(x1, y1, x2, y2) {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
+export function createButton(ctx, x, y, radius, callback) {
+    let touched = false, identifier;
+
+    // Zeichenfunktion
+    function draw() {
+        if (touched) circle(ctx, x, y, radius, "red");
+        else circle(ctx, x, y, radius, "gray");
+    }
+
+    // Berührungfunktion
+    function is_touched(id, tx, ty) {
+        touched = distance(x, y, tx, ty) < radius;
+        if (touched) {
+            identifier = id;
+            callback();
+        }
+    }
+
+    // Reset-Funktione
+    function reset(id) {
+        if (id === identifier) {
+            touched = false;
+            identifier = undefined;
+        }
+    }
+
+    return { draw, is_touched, reset, move: () => { } };
+
+}
+
+export function createButtonFromPath(ctx, x, y, p, sc, callback) {
+    let touched = false, identifier;
+    let M = setTransform(ctx, x, y, 0, sc);  // Matrix M Transformation
+
+    function draw() {
+        ctx.save();
+        // ctx.translate(x, y);
+        // ctx.scale(sc, sc);
+        // M = ctx.getTransform();
+        // if (touched) path(ctx, p, "#f00", "#f00", 1);
+        // else path(ctx, p, "#aaa", "#aaa", 1);
+        if (touched)
+            fillPath(ctx, p, M, "#f00");
+        else
+            fillPath(ctx, p, M, "#f0f");
+
+        ctx.restore();
+    }
+
+    function is_touched(id, tx, ty) {
+        const I = (new DOMMatrix(M)).invertSelf();  // M-1
+        const L = I.transformPoint(new DOMPoint(tx, ty));
+        touched = ctx.isPointInPath(p, L.x, L.y);
+        if (touched) {
+            identifier = id;
+            callback();
+        }
+    }
+
+
+    function reset(id) {
+        if (id === identifier) {
+            touched = false;
+            identifier = undefined;
+        }
+    }
+
+    return { draw, is_touched, reset, move: () => { } };
+
+}
+
+
+export function createGrabbable(ctx, x, y, p, sc) {
+    let touched = false, identifier, P;
+    let L = setTransform(ctx, x, y, 0, sc);  // Matrix M Transformation
+
+    function draw() {
+        ctx.save();
+        // console.log(L.e, L.f);
+        if (touched)
+            fillPath(ctx, p, L, "#f00");
+        else
+            fillPath(ctx, p, L, "#f0f");
+
+        ctx.restore();
+    }
+
+    function is_touched(id, tx, ty) {
+        const I = (new DOMMatrix(L)).invertSelf();  // M-1
+        const transformedTP = I.transformPoint(new DOMPoint(tx, ty));
+        touched = ctx.isPointInPath(p, transformedTP.x, transformedTP.y);
+        if (touched) {
+            identifier = id;
+            P = (new DOMMatrix([1, 0, 0, 1, -tx, -ty])).multiplySelf(L); // Ti-1 Li
+        }
+    }
+
+    function move(id, tx, ty) {
+        if (id === identifier) {
+            // console.log(id, tx, ty);
+            L = (new DOMMatrix([1, 0, 0, 1, tx, ty])).multiplySelf(P);   // Tn P
+        }
+
+    }
+
+    function reset(id) {
+        if (id === identifier) {
+            touched = false;
+            identifier = undefined;
+        }
+    }
+
+    return { draw, is_touched, reset, move };
+
+}
+
+export function createJoystick(ctx, x, y, radius = 60) {
+    let touched = false, identifier;
+
+    const Touchpoints = {};
+
+    // Zeichenfunktion
+    function draw() {
+        if (touched) circle(ctx, x, y, radius, "orange");
+        else circle(ctx, x, y, radius, "red");
+
+        if (Touchpoints.initial !== undefined)
+            circle(ctx, Touchpoints.initial.tx, Touchpoints.initial.ty, 6, "gray");
+        if (Touchpoints.firstExternal !== undefined)
+            circle(ctx, Touchpoints.firstExternal.tx, Touchpoints.firstExternal.ty, 6, "gray");
+    }
+
+    // Berührungfunktion
+    function is_touched(id, tx, ty) {
+        touched = distance(x, y, tx, ty) < radius;
+        if (touched) {
+            identifier = id;
+            Touchpoints.initial = { tx, ty };
+        }
+    }
+
+    function move(id, tx, ty) {
+        if (id === identifier) {
+            // console.log(id, tx, ty);
+            if (Touchpoints.firstExternal === undefined) {
+                const d = distance(x, y, tx, ty);
+                if (d > radius) {
+                    Touchpoints.firstExternal = { tx, ty };
+                }
+            }
+        }
+
+    }
+
+    function reset(id) {
+        if (id === identifier) {
+            touched = false;
+            identifier = undefined;
+            Touchpoints.firstExternal = undefined;
+            Touchpoints.initial = undefined;
+        }
+    }
+
+    return { draw, is_touched, reset, move };
+
+}
+
+
+
+
+export function u_path() {
+    let upath = new Path2D();
+    upath.moveTo(-2, -2);
+    upath.lineTo(-2, 2);
+    upath.lineTo(-1, 2);
+    upath.lineTo(-1, -1);
+    upath.lineTo(1, -1);
+    upath.lineTo(1, 2);
+    upath.lineTo(2, 2);
+    upath.lineTo(2, -2);
+    upath.closePath();
+    return upath;
+}
+
+export function path(ctx, p, fillStyle = "#fff", strokeStyle = "#000", lineWidth = 1) {
+    ctx.fillStyle = fillStyle;
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.fill(p);
+    ctx.stroke(p);
+}
+
+export function fillPath(ctx, path, Matrix, fillStyle = "#fff", strokeStyle = "#000", lineWidth = 0.1) {
+    ctx.save();  // Speichern des Zustands mit der aktuellen Matrix auf Stack
+    ctx.setTransform(Matrix); // Setzen der Matrix
+    ctx.fillStyle = fillStyle;
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.fill(path);
+    ctx.stroke(path);
+    ctx.restore(); // Holen der gespeicherten Matrix vom Stack
+}
+
+
+
+const END_ANGLE = Math.PI * 2;
+
+function circle(ctx, x, y, radius, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, END_ANGLE, true);
+    ctx.fill();
+}
+
+
+export function rect(ctx) {
+    ctx.fillStyle = "#f0f";
+    ctx.fillRect(10, 10, 20, 40);
+}
+
+export function drawStar(ctx, cx, cy, spikes, outerRadius, innerRadius) {
+    let rot = (Math.PI / 2) * 3;
+    const step = Math.PI / spikes;
+
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - outerRadius);
+    for (let i = 0; i < spikes; i++) {
+        ctx.lineTo(cx + Math.cos(rot) * outerRadius, cy + Math.sin(rot) * outerRadius);
+        rot += step;
+
+        ctx.lineTo(cx + Math.cos(rot) * innerRadius, cy + Math.sin(rot) * innerRadius);
+        rot += step;
+    }
+    ctx.lineTo(cx, cy - outerRadius);
+    ctx.closePath();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+    ctx.fillStyle = 'yellow';
+    ctx.fill();
+}
+
+let angle = 0;
+export function drawRotatedRect(ctx) {
+    const rectWidth = 100;
+    const rectHeight = 60;
+
+    ctx.translate(rectWidth, rectHeight);
+    ctx.rotate(angle);
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(-rectWidth / 2, -rectHeight / 2, rectWidth, rectHeight);
+    ctx.fillStyle = 'gray';
+    ctx.fillRect(0, 0, rectWidth, rectHeight);
+    angle += 0.02;
+}
+
+export function drawGradient(ctx) {
+    // ab hier wird in dieser Farbe gefüllt
+    ctx.fillStyle = "#f00";
+
+    // rechteck malen
+    ctx.fillRect(10, 40, 200, 100);
+
+    // rand zeichnen
+    ctx.strokeStyle = "#00f";
+    ctx.lineWidth = 3;
+    ctx.strokeRect(10, 40, 200, 100);
+
+    // Erstellen eines linearen Farbverlaufs
+    const gradient = ctx.createLinearGradient(10, 40, 210, 40); // Horizontaler Verlauf von links nach rechts
+    gradient.addColorStop(0, "red");    // Farbstartpunkt
+    gradient.addColorStop(1, "blue");   // Farbendpunkt
+
+    // Ab hier wird der Farbverlauf als Füllstil verwendet
+    ctx.fillStyle = gradient;
+
+    // Rechteck malen
+    ctx.fillRect(10, 40, 200, 100);
+
+    // Rand zeichnen
+    ctx.strokeStyle = "#00f";
+    ctx.lineWidth = 3;
+    ctx.strokeRect(10, 40, 200, 100);
+}
+
+
+export function setTransform(ctx, x, y, alpha = 0, sc = 1) {
+    ctx.resetTransform(); // Zurücksetzen auf Identität
+    ctx.translate(x, y); // Translation
+    ctx.rotate(alpha); // Rotation
+    ctx.scale(sc, sc); // Skalierung
+    return ctx.getTransform(); // Berechnete Matrix speichern
+}
+
+export function getTransform(ctx, x, y, alpha = 0, sc = 1) {
+    ctx.save();  // Speichern des Zustands mit der aktuellen Matrix auf Stack
+    setTransform(ctx, x, y, alpha, sc);
+    let L = ctx.getTransform(); // Berechnete Matrix speichern
+    ctx.restore(); // Holen der gespeicherten Matrix vom Stack
+    return L; // Rückgabe
+}
---
+++ 06_Grab_two_Fingers/main_js.mjs
@@ -0,0 +1,98 @@
+import * as lib from "./js/canvas_funcs.mjs"
+
+// Aeussere Funktion: anonym/lamda
+// an Eventlistener; wird einmal nach dem Laden aufgerufen
+window.onload = () => {
+    const { canvas, ctx } = lib.init("canvas_id");
+    ctx.fillStyle = "#333";
+
+    const u = lib.u_path();
+
+
+    const interactiveElements = [];
+    const Touches = {}, points = [];
+    let lineColor = "#aaa";
+    interactiveElements.push(lib.createButton(ctx, 100, 100, 50, () => { lineColor = "#f00"; }));
+    interactiveElements.push(lib.createButton(ctx, 200, 100, 50, () => { lineColor = "#0f0"; }));
+    interactiveElements.push(lib.createButton(ctx, 300, 100, 50, () => { points.length = 0; }));
+    // interactiveElements.push(lib.createButtonFromPath(ctx, 400, 100, u, 30, () => { console.log("btn U"); }));
+    interactiveElements.push(lib.createGrabbable(ctx, 400, 200, u, 30));
+    interactiveElements.push(lib.createJoystick(ctx, 400, 400));
+
+
+    canvas.addEventListener("touchstart", (evt) => {
+        evt.preventDefault();
+        for (const t of evt.changedTouches) {
+            Touches[t.identifier] = { x: t.pageX, y: t.pageY };
+            for (const ie of interactiveElements) {
+                ie.is_touched(t.identifier, t.pageX, t.pageY);
+            }
+            points.push({ isStart: true, lineColor, x: t.pageX, y: t.pageY });
+            // console.log(`finger ${t.identifier}: ${t.pageX}, ${t.pageY}`);
+        }
+    });
+
+    canvas.addEventListener("touchend", (evt) => {
+        evt.preventDefault();
+        for (const t of evt.changedTouches) {
+            delete Touches[t.identifier];
+            for (const ie of interactiveElements) {
+                ie.reset(t.identifier);
+            }
+        }
+    });
+
+
+    canvas.addEventListener("touchmove", (evt) => {
+        evt.preventDefault();
+        for (const t of evt.changedTouches) {
+            Touches[t.identifier] = { x: t.pageX, y: t.pageY };
+            for (const ie of interactiveElements) {
+                ie.move(t.identifier, t.pageX, t.pageY);
+            }
+            points.push({ isStart: false, lineColor, x: t.pageX, y: t.pageY });
+        }
+    });
+
+    function drawLines() {
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        for (let p of points) {
+            if (p.isStart) {
+                ctx.stroke();
+                ctx.strokeStyle = p.lineColor;
+                ctx.beginPath();
+                ctx.moveTo(p.x, p.y);
+            } else {
+                ctx.lineTo(p.x, p.y);
+            }
+        }
+        ctx.stroke();
+
+    }
+
+    // Haupt-Zeichnen-Funktion
+    function draw() {
+        ctx.resetTransform();
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+
+        for (const ie of interactiveElements) {
+            ie.draw();
+        }
+
+        const ids = Object.keys(Touches);
+        // console.log(ids);
+        for (const identifier of ids) {
+            const t = Touches[identifier];
+            lib.drawStar(ctx, t.x, t.y, 6, 12, 8);
+        }
+
+        drawLines();
+
+        window.requestAnimationFrame(draw);
+    }
+    draw();
+    const d = new Date().toLocaleString("de-DE");
+    console.log("onload finish", d);
+};
